a9cb9d4f607997119bbdf0fa7b232be7f05703f2
harelpanker <harelpanker@gmail.com>
2023-04-28 08:04:49 +0200

    start hero #2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Hero from '@/components/home-page/Hero';
-import Container from '@/components/ui/Container';
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-center relative py-20'>
-      <Container>
-        <Hero />
-      </Container>
+    <div className='flex flex-col justify-center relative py-12 lg:py-20'>
+      <Hero />
     </div>
   );
 }
--- components/home-page/Hero.tsx
+++ components/home-page/Hero.tsx
@@ -7,16 +7,18 @@ type HeroProps = {};
 
 const Hero: FC<HeroProps> = ({}) => {
   return (
-    <header className='flex flex-col gap-6 items-center lg:py-28'>
+    <header className='flex flex-col gap-6 items-center lg:py-28 lg:px-12'>
       <Container>
-        <div className='grid'>
-          <div className='relative'>
+        <div className='flex flex-col xl:grid grid-cols-8 grid-rows-4'>
+          <div className='relative mb-8 xl:mb-0 col-span-4 row-span-3 col-start-2 row-start-2 grayscale hover:grayscale-0 transition duration-300'>
             <Image src={hero_image} alt='Harel Panker - Fron end developer' />
-            <h1 className='text-4xl font-serif md:text-7xl xl:text-[18rem] 2xl:text-[22rem] font-bold text-transparent bg-clip-text bg-gradient-to-br from-slate-900 via-slate-800 to-slate-500'>
-              <span>Hello</span>
-              <span className='block'>I&apos;m Harel</span>
-            </h1>
           </div>
+          <h1 className='static xl:absolute mix-blend-multiply z-10 text-7xl font-serif md:text-9xl xl:text-[16rem] 2xl:text-[22rem] font-bold'>
+            <span>Hello</span>
+            <span className='block xl:ml-32 whitespace-nowrap'>
+              I&apos;m Harel
+            </span>
+          </h1>
         </div>
       </Container>
     </header>
