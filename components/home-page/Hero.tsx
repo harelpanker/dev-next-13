c79b732b81a9ab652c1a0a117c00486635ac6717
harelpanker <harelpanker@gmail.com>
2023-05-06 21:44:22 +0200

    basic

--- components/home-page/Hero.tsx
+++ components/home-page/Hero.tsx
@@ -1,19 +1,45 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import Image from 'next/image';
 import hero_image from '../../public/images/2.jpeg';
 import Container from '../ui/Container';
 import Link from 'next/link';
+import { animate, inView } from 'motion';
 
 type HeroProps = {};
 
 const Hero: FC<HeroProps> = ({}) => {
+  useEffect(() => {
+    animate(
+      '.span_hello',
+      { opacity: [0, 1], y: ['40%', '0%'] },
+      { duration: 1.5 }
+    );
+    animate(
+      '.span_name',
+      { opacity: [0, 1], y: ['20%', '0%'] },
+      { duration: 1.5, delay: 0.5 }
+    );
+    animate(
+      '.figure',
+      { opacity: [0, 1], y: ['30%', '0%'] },
+      { duration: 1.5, delay: 0.5 }
+    );
+    inView('.bottom_content', (info) => {
+      animate(
+        info.target,
+        { opacity: [0, 1], y: ['20%', '0%'] },
+        { delay: 0.5, duration: 0.9 }
+      );
+    });
+  }, []);
+
   return (
     <section className='flex flex-col gap-6 items-center lg:py-28 lg:px-12'>
       <Container>
         <div className='flex flex-col xl:grid grid-cols-8 grid-rows-4'>
-          <figure className='relative mb-8 xl:mb-0 col-span-4 row-span-3 col-start-2 row-start-2 grayscale hover:grayscale-0 transition duration-300'>
+          <figure className='figure relative mb-8 xl:mb-0 col-span-4 row-span-3 col-start-2 row-start-2 grayscale hover:grayscale-0 transition duration-300'>
             <Image
               priority
               src={hero_image}
@@ -22,12 +48,12 @@ const Hero: FC<HeroProps> = ({}) => {
             />
           </figure>
           <h1 className='static xl:absolute mix-blend-multiply z-10 text-7xl font-serif md:text-9xl xl:text-[16rem] 2xl:text-[22rem] font-bold'>
-            <span>Hello</span>
-            <span className='block xl:ml-36 whitespace-nowrap'>
+            <span className='span_hello block'>Hello</span>
+            <span className='span_name block xl:ml-36 whitespace-nowrap'>
               I&apos;m Harel
             </span>
           </h1>
-          <div className='row-start-4 col-start-6 col-span-3 flex flex-col xl:p-6 max-w-xl justify-end items-start gap-3 text-xl md:text-2xl font-medium'>
+          <div className='bottom_content row-start-4 col-start-6 col-span-3 flex flex-col xl:p-6 max-w-xl justify-end items-start gap-3 text-xl md:text-2xl font-medium'>
             <p>
               A front-end engineer working as a freelancer with leading agencies
               and startups, developing products that meet the users&apos; needs.
