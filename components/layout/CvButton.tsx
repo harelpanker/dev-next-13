c6be242bc4d6976a3160692d343434c360f2aba6
harelpanker <harelpanker@gmail.com>
2023-05-11 22:12:15 +0200

    cv link

--- components/layout/CvButton.tsx
+++ components/layout/CvButton.tsx
@@ -7,7 +7,7 @@ const CvButton: FC<CvButtonProps> = ({}) => {
   return (
     <a
       className='fixed z-50 whitespace-pre top-auto left-auto bottom-5 right-5 xl:right-[5vw] font-medium flex gap-2 items-center rounded-full px-4 py-1 bg-purple-300 text-purple-700 transition hover:bg-purple-700 hover:text-slate-50'
-      href='https://drive.google.com/file/d/1p7XXNsJm22Cts7lMUIx7AvGNBP0nIM6Y/view?usp=sharing'
+      href='https://drive.google.com/file/d/1DpwfkAxD1QdB239tj2nq_GFGR3kz5uwl/view?usp=share_link'
       target='_blank'
       download='Harel Panker CV - 2021'
       rel='noopener noreferrer'>
