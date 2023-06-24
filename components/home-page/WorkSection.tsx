3e4613a7f3982b41db7c778a3e7c945becdd3a38
harelpanker <harelpanker@gmail.com>
2023-06-24 15:51:37 +0200

    work section

--- components/home-page/WorkSection.tsx
+++ components/home-page/WorkSection.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import Image from 'next/image';
 
 import Container from '../ui/Container';
@@ -9,12 +10,25 @@ import { Plus } from 'lucide-react';
 import * as Accordion from '@radix-ui/react-accordion';
 
 import noise from '/public/images/noise.webp';
-import site1 from '/public/images/sites/datree.png';
+import datree from '/public/images/sites/datree.png';
+
+import styles from './styles.module.css';
+import { workData } from '@/utils/work_data';
 
 const WorkSection = () => {
+  const [order, setOrder] = useState({
+    id: '1',
+    image: datree,
+    name: 'datree',
+  });
+
+  const handleImageOrder = (id: string, image: any, name: string) => {
+    setOrder({ id: id, image: image, name: name });
+  };
+
   return (
     <>
-      <section className='noise-work relative w-full'>
+      <section className='noise-work relative w-full px-5 lg:px-12'>
         <Container>
           <div className='flex flex-col gap-12'>
             <header className='flex flex-col gap-4 md:gap-10 text-center items-center'>
@@ -26,36 +40,95 @@ const WorkSection = () => {
             </header>
 
             {/* main section content */}
-            <ul>
+            <div className='w-full max-w-screen-xl mx-auto relative lg:gap-20 lg:flex lg:items-center lg:justify-between'>
               <Accordion.Root
-                className='AccordionRoot'
+                className='grid gap-4 mx-auto lg:mx-0 md:w-[380px] lg:shrink-0'
                 type='single'
                 defaultValue='item-1'>
-                <Accordion.Item className='AccordionItem' value='item-1'>
-                  <Accordion.Header>
-                    <Accordion.Trigger className='flex justify-between gap-4 items-center'>
-                      <span>Datree.io</span>
-                      <Plus />
-                    </Accordion.Trigger>
-                  </Accordion.Header>
-                  <Accordion.Content>
-                    <Image src={site1} alt='Datree.io' />
-                  </Accordion.Content>
-                </Accordion.Item>
+                {/* item */}
+                {workData.map((item) => (
+                  <div key={item.id} className='accordion_item'>
+                    <Accordion.Item
+                      className='p-4 group flex flex-col'
+                      value={`item-${item.id}`}>
+                      {/*  */}
+                      {/* header  */}
+                      <Accordion.Header
+                        className={`${styles.accordion_header}`}>
+                        <Accordion.Trigger
+                          onClick={() =>
+                            handleImageOrder(item.id, item.image, item.name)
+                          }
+                          className='flex justify-between gap-4 items-center w-full text-2xl font-medium cursor-pointer'>
+                          <span>{item.name}</span>
+                          <Plus className='transition duration-300' />
+                        </Accordion.Trigger>
+                      </Accordion.Header>
+                      {/* content */}
+                      <Accordion.Content
+                        className={`gap-6 flex flex-col overflow-hidden ${styles.accordion_content} data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp`}>
+                        <div>
+                          <p className='text-sm md:text-base mb-3'>
+                            {item.description}
+                          </p>
+                          <div className='flex justify-end'>
+                            <a
+                              className='underline font-medium'
+                              target='_blank'
+                              rel='noreferrer'
+                              href={item.link}>
+                              See it in the wild
+                            </a>
+                          </div>
+                        </div>
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          className='lg:hidden'
+                        />
+                      </Accordion.Content>
+                      {/*  */}
+                    </Accordion.Item>
+                  </div>
+                ))}
               </Accordion.Root>
-            </ul>
+              <div className='hidden lg:block lg:w-full'>
+                <figure>
+                  <Image
+                    quality={100}
+                    className='max-w-[1000px]'
+                    src={order.image}
+                    alt={order.name}
+                  />
+                </figure>
+              </div>
+            </div>
           </div>
         </Container>
       </section>
       <style jsx>{`
-        .noise-work {
+        .accordion_item {
+          position: relative;
+          isolation: isolate;
+          border-width: 1px;
+          border-color: rgb(52 56 65);
+          background-color: rgb(35 38 45);
+        }
+        .accordion_item::before {
+          position: absolute;
+          inset: 0;
+          z-index: -20;
+          opacity: 0.4;
+          mix-blend-mode: overlay;
+          content: '';
+          background-image: url(${noise.src});
         }
         .noise-work::before {
           position: absolute;
           content: '';
           z-index: -1;
           top: 15%;
-          height: 60%;
+          height: 80%;
           width: 150%;
           left: -25%;
           background: url(${noise.src}) repeat,
---
+++ utils/work_data.tsx
@@ -0,0 +1,48 @@
+import datree from '/public/images/sites/datree.png';
+import permit from '/public/images/sites/permit.png';
+import yonivers from '/public/images/sites/yonivers.png';
+import wordtune from '/public/images/sites/wordtune.png';
+import lin from '/public/images/sites/lin.png';
+
+export const workData = [
+  {
+    id: '1',
+    name: 'Datree.io',
+    link: 'https://www.datree.io',
+    image: datree,
+    description:
+      'Datree secures your Kubernetes by blocking the deployment of misconfigured resources. The site was built with Webflow and the doc site with Docusaurus (react framework for documentation sites).',
+  },
+  {
+    id: '2',
+    name: 'Permit.io',
+    link: 'https://www.permit.io/',
+    image: permit,
+    description:
+      'Plug & Play App-Level Authorization. Enforce in Frontend or Backend, control via No-Code UI Elements. built with NextJS 13 connected to headless CMS.',
+  },
+  {
+    id: '3',
+    name: 'Yonivers',
+    link: 'https://app.yonivers.com/mrh/1',
+    image: yonivers,
+    description:
+      'Insurance | Simple, Rapide, Facile. Complex multi-step form built with NextJS and React Hook Form.',
+  },
+  {
+    id: '4',
+    name: 'Wordtune.com',
+    link: 'https://www.wordtune.com/teams/home',
+    image: wordtune,
+    description:
+      'AI writing assistant for any business need. Built with Webflow along side to ai21.com/studio.',
+  },
+  {
+    id: '5',
+    name: 'lin.Health',
+    link: 'https://join.lin.health/lp/sarno',
+    image: lin,
+    description:
+      'A new approach to pain recovery. The site was built with Webflow and some of the landing pages with NextJS.',
+  },
+];
