366b2e950dd02dabcbb607559c4d7fa1ac76cd48
harelpanker <harelpanker@gmail.com>
2023-06-27 20:25:41 +0200

    small fix

--- app/(home-components)/Hero.tsx
+++ app/(home-components)/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
             </div>
             <a
               className='relative z-10 overflow-hidden p-1 rounded-full group hover:-translate-y-2 transition duration-500'
-              href='/#about'>
+              href='/#skills'>
               <span className='text-theme_white bg-clip-text bg-gradient-to-br from-[#3245ff] to-[#bc52ee] md:text-lg font-medium relative z-30 w-full px-6 md:px-10 py-2 md:py-3 h-full flex justify-center items-center'>
                 Skills
               </span>
